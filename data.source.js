41828ac6ddcc03eb9ef8afcc0b582069400885cb
11adds <86661521+11adds@users.noreply.github.com>
2022-04-04 19:16:19 +0800

    Add files via upload

---
+++ data.source.js
@@ -0,0 +1,462 @@
+import React from 'react';
+export const Content110DataSource = {
+  OverPack: {
+    className: 'home-page-wrapper content11-wrapper',
+    playScale: 0.3,
+  },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'image',
+        children:
+          'https://gw.alipayobjects.com/zos/rmsportal/PiqyziYmvbgAudYfhuBr.svg',
+        className: 'title-image',
+      },
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>DeewinStudio网络安全应急响应中心</p>
+          </span>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        children: (
+          <span>
+            <p>为机密进行防护，保证工作室安全</p>
+          </span>
+        ),
+        className: 'title-content',
+      },
+      {
+        name: 'content2',
+        children: (
+          <span>
+            <p>对所有人开放，反馈您的意见或上报您发现的漏洞</p>
+          </span>
+        ),
+        className: 'title-content',
+      },
+    ],
+  },
+  button: {
+    className: '',
+    children: {
+      a: {
+        className: 'button',
+        href:
+          'https://jq.qq.com/?_wv=1027&k=ajoVnGai                                                                                             ',
+        children: (
+          <span>
+            <span>
+              <p>立即反馈</p>
+            </span>
+          </span>
+        ),
+        target: '_blank',
+      },
+    },
+  },
+};
+export const Content30DataSource = {
+  wrapper: {
+    className: 'home-page-wrapper content3-wrapper l1k2iikss-editor_css',
+  },
+  page: { className: 'home-page content3' },
+  OverPack: { playScale: 0.3 },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>为工作室网络安全提供服务</p>
+          </span>
+        ),
+        className: 'title-h1',
+      },
+      {
+        name: 'content',
+        className: 'title-content',
+        children: (
+          <span>
+            <p>360，火绒安全，防火墙实时防护监控</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  block: {
+    className: 'content3-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/ScHBSdwpTkAHZkJ.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <span>
+                  <span>
+                    <p>团队网络安全管理</p>
+                  </span>
+                </span>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>
+                  由DeewinStudio应急事件部与DeewinStudio网络安全管理部进行督察
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/NKBELAOuuKbofDD.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: {
+            className: 'content3-title',
+            children: (
+              <span>
+                <p>安全</p>
+              </span>
+            ),
+          },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>DeewinStudio全方位保障定制应用安全无毒及系统数据安全。</p>
+              </span>
+            ),
+          },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content3-block',
+        md: 8,
+        xs: 24,
+        children: {
+          icon: {
+            className: 'content3-icon',
+            children:
+              'https://zos.alipayobjects.com/rmsportal/xMSBjgxBhKfyMWX.png',
+          },
+          textWrapper: { className: 'content3-text' },
+          title: { className: 'content3-title', children: '云监控' },
+          content: {
+            className: 'content3-content',
+            children: (
+              <span>
+                <p>
+                  主管理部门通过软件进行监控，统一管理与审核项目，智能分析及故障定位。
+                </p>
+              </span>
+            ),
+          },
+        },
+      },
+    ],
+  },
+};
+export const Feature80DataSource = {
+  wrapper: { className: 'home-page-wrapper feature8-wrapper' },
+  page: { className: 'home-page feature8' },
+  OverPack: { playScale: 0.3 },
+  titleWrapper: {
+    className: 'feature8-title-wrapper',
+    children: [
+      {
+        name: 'title',
+        className: 'feature8-title-h1',
+        children: (
+          <span>
+            <p>加入我们</p>
+          </span>
+        ),
+      },
+      {
+        name: 'content',
+        className: 'feature8-title-content',
+        children: (
+          <span>
+            <p>加入我们吧！</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  childWrapper: {
+    className: 'feature8-button-wrapper',
+    children: [
+      {
+        name: 'button',
+        className: 'feature8-button',
+        children: {
+          href: 'https://jq.qq.com/?_wv=1027&k=lWPgDX16 ',
+          children: (
+            <span>
+              <span>
+                <p>加入我们</p>
+              </span>
+            </span>
+          ),
+          target: '_blank',
+        },
+      },
+    ],
+  },
+  Carousel: {
+    dots: false,
+    className: 'feature8-carousel',
+    wrapper: { className: 'feature8-block-wrapper' },
+    children: {
+      className: 'feature8-block',
+      titleWrapper: {
+        className: 'feature8-carousel-title-wrapper',
+        title: { className: 'feature8-carousel-title' },
+      },
+      children: [
+        {
+          name: 'block0',
+          className: 'feature8-block-row',
+          gutter: 120,
+          title: {
+            className: 'feature8-carousel-title-block',
+            children: (
+              <span>
+                <span>
+                  <p>加入我们</p>
+                </span>
+              </span>
+            ),
+          },
+          children: [
+            {
+              className: 'feature8-block-col',
+              md: 6,
+              xs: 24,
+              name: 'child0',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+              },
+              children: {
+                className: 'feature8-block-child',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: (
+                      <span>
+                        <span>
+                          <span>
+                            <span>
+                              <p>联系我们</p>
+                            </span>
+                          </span>
+                        </span>
+                      </span>
+                    ),
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children: (
+                      <span>
+                        <span>
+                          <p>加QQ：3546467580以便联系</p>
+                        </span>
+                      </span>
+                    ),
+                  },
+                ],
+              },
+            },
+            {
+              className: 'feature8-block-col',
+              md: 6,
+              xs: 24,
+              name: 'child1',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+              },
+              children: {
+                className: 'feature8-block-child',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: (
+                      <span>
+                        <span>
+                          <p>一次技能测试</p>
+                        </span>
+                      </span>
+                    ),
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children: (
+                      <span>
+                        <span>
+                          <p>对指定内容进行测试</p>
+                        </span>
+                      </span>
+                    ),
+                  },
+                ],
+              },
+            },
+            {
+              className: 'feature8-block-col',
+              md: 6,
+              xs: 24,
+              name: 'child2',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+              },
+              children: {
+                className: 'feature8-block-child',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: (
+                      <span>
+                        <span>
+                          <p>一次综合测试</p>
+                        </span>
+                      </span>
+                    ),
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children: (
+                      <span>
+                        <p>对所有考试内容整理测试</p>
+                      </span>
+                    ),
+                  },
+                ],
+              },
+            },
+            {
+              className: 'feature8-block-col',
+              md: 6,
+              xs: 24,
+              name: 'child3',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+              },
+              children: {
+                className: 'feature8-block-child',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: (
+                      <span>
+                        <span>
+                          <span>
+                            <p>指导通过</p>
+                          </span>
+                        </span>
+                      </span>
+                    ),
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children: (
+                      <span>
+                        <p>对擅长内容进行指导，之后通过</p>
+                      </span>
+                    ),
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <h2>
+          <span>
+            ©2018 <b>DeewinStudio</b>&nbsp;All Rights Reserved
+          </span>
+        </h2>
+      </span>
+    ),
+  },
+};
